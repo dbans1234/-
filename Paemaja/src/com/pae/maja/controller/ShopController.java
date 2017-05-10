1327f77969868103371b53fd794394f9cc6585a7
EDU-A23 <EDU-A23@10.10.2.62>
2017-05-10 13:36:24 +0900

    ShopDAO add

---
+++ Paemaja/src/com/pae/maja/controller/ShopController.java
@@ -0,0 +1,26 @@
+package com.pae.maja.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by SungHere on 2017-04-17.
+ */
+@Controller
+public class ShopController {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(ShopController.class);
+
+    @RequestMapping(value = "write.do", method = RequestMethod.GET)
+    public String write(Model model) throws Exception {
+
+        logger.info("Welcome HomeController write---------------------------------!");
+        return "write.tiles";
+    }
+
+}
